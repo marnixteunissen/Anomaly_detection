73ac2fee7acd4f1cb303d304fd67166dbd799368
marnixteunissen <70704360+marnixteunissen@users.noreply.github.com>
2021-05-25 16:29:14 +0200

    for evaluation on PC

--- predict.py
+++ predict.py
@@ -17,10 +17,6 @@ def draw_label(img, text):
 if __name__ == "__main__":
     class_names = {0: "FJOK", 1: "NONE"}
     exp_dir = os.path.abspath(r'runs/Varying layers and filters/25')
-    # f = open((exp_dir+'/config.json'))
-    # exp_para = json.load(f)
-    # filters = exp_para['n_filters']
-    # layers = exp_para['n_layers']
     model = tf.keras.models.load_model(exp_dir+'/saved_model')
     model.summary()
 
@@ -33,30 +29,30 @@ if __name__ == "__main__":
     # model.summary()
     # model.load_weights((exp_dir + r'/best_weights.ckpt'))
 
-    cap = cv2.VideoCapture('20200423173206224@MainDVR_Ch2.mp4')
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        model_input = tf.expand_dims(cv2.resize(frame, (480, 360)), 0)
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        # Run inference on frame:
-        pred = model(model_input)
-        print(pred.numpy())
-        prob = tf.nn.softmax(pred)
-        print('probabilities: ', prob)
-        pred = np.argmax(pred.numpy())
-        label = class_names[pred]
-        frame = draw_label(frame, (label+'({}%)'.format(prob*100)))
-        # Display the resulting frame
-        cv2.imshow('Feed', frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap = cv2.VideoCapture('20200423173206224@MainDVR_Ch2.mp4')
+    # if not cap.isOpened():
+    #     print("Cannot open camera")
+    #     exit()
+    # while True:
+    #     # Capture frame-by-frame
+    #     ret, frame = cap.read()
+    #     model_input = tf.expand_dims(cv2.resize(frame, (480, 360)), 0)
+    #     # if frame is read correctly ret is True
+    #     if not ret:
+    #         print("Can't receive frame (stream end?). Exiting ...")
+    #         break
+    #     # Run inference on frame:
+    #     pred = model(model_input)
+    #     print(pred.numpy())
+    #     prob = tf.nn.softmax(pred)
+    #     print('probabilities: ', prob)
+    #     pred = np.argmax(pred.numpy())
+    #     label = class_names[pred]
+    #     frame = draw_label(frame, (label+'({}%)'.format(prob*100)))
+    #     # Display the resulting frame
+    #     cv2.imshow('Feed', frame)
+    #     if cv2.waitKey(1) == ord('q'):
+    #         break
+    # # When everything done, release the capture
+    # cap.release()
+    # cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -16,13 +16,14 @@ def evaluate_network(model, data_dir, channel='TOP'):
 def eval_show_false(model, data_dir, channel='TOP'):
     class_names = {0: "FJOK", 1: "NONE"}
     test_set = create_test_set(data_dir, channel)
-    result = model.evaluate(test_set)
-    i = 1
+    # result = model.evaluate(test_set)
+
     for image, label in test_set:
         label = label.numpy()[0]
         pred = model(image)
         prob = tf.nn.softmax(pred)
         pred = np.argmax(model(image).numpy())
+
         if pred != label:
             print('label:', label)
             print('Prediction: {} ({}%)'.format(pred, prob.numpy()[0][pred]*100))
@@ -32,22 +33,15 @@ def eval_show_false(model, data_dir, channel='TOP'):
                                                                class_names[pred],
                                                                prob.numpy()[0][pred]),
                        image.numpy().astype("uint8"))
-            cv2.waitKey(2000)
-        i = i+1
-        if i == 10:
-            cv2.waitKey(1)
-            break
-
+            cv2.imwrite(r'\false_labeled\{}_{}.png'.format(class_names[pred], prob.numpy()[0][pred]),
+                        image.numpy().astype("uint8"))
+            if cv2.waitKey(1) == ord('q'):
+                continue
 
 
 if __name__ == "__main__":
-    data_dir = r'E:\Anomaly_detection'
-    train_set, val_set = data_processing.create_data_sets(data_dir, 'TOP', 'train')
-
-    test_model = build_conv_network(3, 16)
-    # test_model.fit(train_set, validation_data=val_set, epochs=5)
-    # evaluate_network(test_model, data_dir)
-    # eval_show_false(test_model, data_dir)
+    data_dir = r'C:\Users\MTN\Documents\Survey_anomaly_detection\pycharm\Anomaly_detection\data\data-set'
     exp_dir = os.path.abspath(r'runs/Varying layers and filters/25')
     model_load = tf.keras.models.load_model(exp_dir+'/saved_model')
-    evaluate_network(model_load, data_dir)
+    # evaluate_network(model_load, data_dir)
+    eval_show_false(model_load, data_dir)
