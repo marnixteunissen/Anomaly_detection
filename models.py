f259f6f982d5696c1f23652d326f40ae3e25cdbb
marnixteunissen <70704360+marnixteunissen@users.noreply.github.com>
2021-06-22 15:05:14 +0200

    added ResNet architecture and tested training functions for it

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.models import Model
 from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense, MaxPooling2D, Conv2D, Flatten, \
+    BatchNormalization, Activation, GlobalAveragePooling2D, DepthwiseConv2D, Softmax, \
+    Dropout, ReLU, Concatenate, Conv2DTranspose, Input, Add, AveragePooling2D, ZeroPadding2D
 import tensorflow.keras.losses as losses
+from tensorflow.keras.regularizers import l2
 import numpy as np
 import matplotlib.pyplot
 import data_processing
@@ -59,13 +64,121 @@ def VGG_like_network(num_layers, filters, image_size=(640, 360), kernel=5, class
     return model
 
 
+class ResNet():
+    def __init__(self, num_blocks, num_layers, num_filters=[32, 64], input_shape=[360, 640, 3], activation='relu',
+                 num_classes=2, optimizer='adam'):
+        self.activation = activation
+        self.num_filters = num_filters
+        print(self.num_filters)
+        self.num_blocks = num_blocks
+        self.num_layers = num_layers
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.optim = optimizer
+
+    def res_conv(self, x, s, filters):
+        '''
+        here the input size changes'''
+        x_skip = x
+        f1, f2 = filters
+
+        # first block
+        x = Conv2D(f1, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_regularizer=l2(0.001))(x)
+        # when s = 2 then it is like downsizing the feature map
+        x = BatchNormalization()(x)
+        x = Activation(self.activation)(x)
+
+        # second block
+        x = Conv2D(f1, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_regularizer=l2(0.001))(x)
+        x = BatchNormalization()(x)
+        x = Activation(self.activation)(x)
+
+        # third block
+        x = Conv2D(f2, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+        x = BatchNormalization()(x)
+
+        # shortcut
+        x_skip = Conv2D(f2, kernel_size=(1, 1), strides=(s, s), padding='valid', kernel_regularizer=l2(0.001))(x_skip)
+        x_skip = BatchNormalization()(x_skip)
+
+        # add
+        x = Add()([x, x_skip])
+        x = Activation(self.activation)(x)
+
+        return x
+
+    def res_identity(self, x, filters):
+        # ReNet block where dimension does not change.
+        # The skip connection is just simple identity connection
+        # we will have 3 blocks and then input will be added
+
+        x_skip = x # this will be used for addition with the residual block
+        f1, f2 = filters
+
+        # first block
+        x = Conv2D(f1, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+        x = BatchNormalization()(x)
+        x = Activation(self.activation)(x)
+
+        # second block # bottleneck (but size kept same with padding)
+        x = Conv2D(f1, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_regularizer=l2(0.001))(x)
+        x = BatchNormalization()(x)
+        x = Activation(self.activation)(x)
+
+        # third block activation used after adding the input
+        x = Conv2D(f2, kernel_size=(1, 1), strides=(1, 1), padding='valid', kernel_regularizer=l2(0.001))(x)
+        x = BatchNormalization()(x)
+        # x = Activation(activations.relu)(x)
+
+        # add the input
+        x = Add()([x, x_skip])
+        x = Activation(self.activation)(x)
+
+        return x
+
+    def build_model(self):
+        # First stage:
+        input_im = Input(shape=(self.input_shape[0], self.input_shape[1], self.input_shape[2]))
+        x = ZeroPadding2D(padding=(3, 3))(input_im)
+        x = Conv2D(self.num_filters[0], kernel_size=7, strides=2, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation(self.activation)(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+        num_filters = self.num_filters
+        num_layers = self.num_layers
+
+        # Create blocks
+        for block in range(self.num_blocks-1):
+            x = self.res_conv(x, s=1, filters=num_filters)
+            for layer in range(num_layers):
+                x = self.res_identity(x, filters=num_filters)
+            num_filters = [f*2 for f in num_filters]
+            num_layers += 1
+        # Last layers:
+        x = AveragePooling2D((2, 2), padding='same')(x)
+        x = Flatten()(x)
+        x = Dense(self.num_classes, kernel_initializer='he_normal')(x)
+        x = Softmax()(x)
+        # Define the model:
+        model = Model(inputs=input_im, outputs=x, name='Resnet')
+        model.compile(loss=losses.SparseCategoricalCrossentropy(),
+                      optimizer=self.optim,
+                      metrics=['accuracy'])
+        return model
+
+
 if __name__ == "__main__":
     data_dir = r'E:\Anomaly_detection'
     train_set, val_set = data_processing.create_data_sets(data_dir, 'TOP', 'train', batch_size=8)
     num_classes = len(train_set.class_names)
-
-    model = VGG_like_network(5, [16, 32, 64, 128, 256])
-
+    net_model = NetworkModel()
+    input_shape = [360, 640, 3]
+    resnet = ResNet(2, 2)
+    model = resnet.build_model()
     model.summary()
-
+    # model = build_conv_network(2, 16)
+    # model.summary()
+    # model = VGG_like_network(5, [16, 32, 64, 128, 256])
+    print(train_set)
     history = model.fit(train_set, validation_data=val_set, epochs=1)
--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def run_layer_filter_experiments(layers, filters, image_size, batch_size, kernel
             mode='max',
             save_best_only=True)
         # add early stopping criterion:
-        early_stop = tf.keras.callbacks.EarlyStopping(patience=3)
+        early_stop = tf.keras.callbacks.EarlyStopping(patience=10)
         # start training:
         history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[save_best, early_stop])
         model.save(os.path.join(ex.observers[0].dir, 'saved_model/'))
@@ -156,7 +156,9 @@ def run_layer_filter_experiments(layers, filters, image_size, batch_size, kernel
 
         i = i+1
 
-def run_VGG_experiments(layers, filters, image_size, batch_size, kernels, data_dir=None, out_dir=os.getcwd(), optimizer='adam', epochs=3):
+
+def run_VGG_experiments(layers, filters, image_size, batch_size, kernels, data_dir=None, out_dir=os.getcwd(),
+                        optimizer='adam', epochs=3):
     if data_dir is None:
         data_dir = os.getcwd() + r'\data\data-set'
     train_data, val_data = data_processing.create_data_sets(data_dir, 'TOP', 'train', batch_size)
@@ -187,8 +189,8 @@ def run_VGG_experiments(layers, filters, image_size, batch_size, kernels, data_d
         return train_data, val_data
 
     @ex.capture
-    def build_model(n_layers, filters, image_size):
-        model = models.VGG_like_network(n_layers, filters, image_size=image_size)
+    def build_model(n_layers, filters, kernel, image_size):
+        model = models.VGG_like_network(n_layers, filters, image_size=image_size, kernel=kernel)
         return model
 
     @ex.capture
@@ -196,12 +198,12 @@ def run_VGG_experiments(layers, filters, image_size, batch_size, kernels, data_d
         # create callback to save best model:
         save_best = tf.keras.callbacks.ModelCheckpoint(
             filepath=os.path.join(ex.observers[0].dir, 'saved_model/best'),
-            save_weights_only=True,
+            save_weights_only=False,
             monitor='val_accuracy',
             mode='max',
             save_best_only=True)
         # create callback for early stopping:
-        early_stop = tf.keras.callbacks.EarlyStopping(patience=3)
+        early_stop = tf.keras.callbacks.EarlyStopping(patience=10)
         # start training:
         history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[save_best, early_stop])
         model.save(os.path.join(ex.observers[0].dir, 'saved_model/'))
@@ -274,18 +276,152 @@ def run_VGG_experiments(layers, filters, image_size, batch_size, kernels, data_d
         i = i+1
 
 
+def run_ResNet_experiments(blocks, layers, filters, image_size, batch_size, kernels=3, data_dir=None, out_dir=os.getcwd(),
+                        optimizer='adam', epochs=3):
+    if data_dir is None:
+        data_dir = os.getcwd() + r'\data\data-set'
+    train_data, val_data = data_processing.create_data_sets(data_dir, 'TOP', 'train', batch_size)
+    run_path = os.path.join(out_dir, 'runs')
+
+    ex = Experiment('ResNet architectures')
+    ex.observers.append(FileStorageObserver(basedir=os.path.join(run_path, ex.path)))
+    # loss_path = os.path.join(run_path, ex.path, 'losses.png')
+
+    @ex.config
+    def config():
+        """This is the configuration of the experiment"""
+        dataset_name = 'Anomaly Detection'
+        net_architecture = 'ResNet'
+        data_dir = os.getcwd() + r'\data\data-set'
+        train_ds = []
+        val_ds = []
+        n_layers = []
+        n_filters = []
+        image_size = []
+        batch_size = []
+        optimizer = 'adam'
+        epochs = []
+
+    @ex.capture
+    def data(image_size, batch_size):
+        train_data, val_data = data_processing.create_data_sets(data_dir, 'TOP', 'train', batch_size, image_size=image_size)
+        return train_data, val_data
+
+    @ex.capture
+    def build_model(n_blocks, n_layers, filters, image_size):
+        resnet = models.ResNet(n_blocks, n_layers, filters)
+        model = resnet.build_model()
+        model.summary()
+        return model
+
+    @ex.capture
+    def train(model, train_ds, val_ds, epochs):
+        # create callback to save best model:
+        save_best = tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join(ex.observers[0].dir, 'saved_model/best'),
+            save_weights_only=False,
+            monitor='val_accuracy',
+            mode='max',
+            save_best_only=True)
+        # create callback for early stopping:
+        early_stop = tf.keras.callbacks.EarlyStopping(patience=10)
+        # start training:
+        history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[save_best, early_stop])
+        model.save(os.path.join(ex.observers[0].dir, 'saved_model/'))
+
+        return history
+
+    @ex.capture
+    def save_losses(history):
+        plt.figure()
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['training', 'validation'], loc='upper right')
+        plt.savefig(os.path.join(ex.observers[0].dir, 'losses.png'))
+        plt.clf()
+
+    @ex.capture
+    def test(model, image_size):
+        test_set = data_processing.create_test_set(data_dir, channel='TOP', image_size=image_size)
+        result = model.evaluate(test_set)
+        return result
+
+    @ex.main
+    def main():
+        # Get data
+        print('Creating data-sets...')
+        train_ds, val_ds = data()
+
+        # build network
+        print('Building model...')
+        model = build_model()
+
+        # train network
+        print('Training network:')
+        history = train(model, train_ds, val_ds)
+
+        # Save plot with losses
+        print('Saving losses...')
+        save_losses(history)
+        print('Final accuracy: ', history.history['val_accuracy'][-1])
+
+        # run test
+        test(model)
+
+    i = 1
+    experiments = {}
+    experiments['layers'] = layers
+    experiments['filters'] = filters
+    experiments['kernels'] = kernels
+    experiments['blocks'] = blocks
+
+
+    results = {}
+
+    for l, f, b in zip(experiments['layers'], experiments['filters'], experiments['blocks']):
+        print(f)
+        assert(len(f) == 2)
+        print('blocks: {}, layers: {}, filters: {}'.format(b, l, f))
+        conf = {'n_layers': int(l),
+                'filters': f,
+                'n_blocks': b,
+                'image_size': image_size,
+                'batch_size': batch_size,
+                'epochs': epochs}
+
+        exp_finish = ex.run(config_updates=conf)
+
+        results['layers: {}, filters: {}'.format(l, f)] = exp_finish.result
+        # print('run {}/{} complete'.format(i,len(experiments)))
+
+        i = i+1
+
+
+
+
 if __name__ == "__main__":
-    layersvgg = [5, 6]
-    filtersvgg = [[64, 128, 256, 512, 512], [64, 128, 256, 512, 512]]
-    layers = [10]
-    filters = [64]
-    kernels = [5, 7]
-    CNN = True
-    VGG = True
+    layersvgg = [5, 5]
+    filtersvgg = [[16, 16, 32, 64, 128], [16, 16, 32, 64, 128]]
+    layers = [6]
+    filters = [32]
+    kernels = [7]
+
+    num_blocks = [4]
+    num_layers = [3]
+    num_filters = [[32, 64]]
+    CNN = False
+    VGG = False
+    ResNet = True
 
     if CNN:
         run_layer_filter_experiments(layers, filters, kernels=kernels, image_size=(640, 360), batch_size=32,
-                                     data_dir=r'E:\Anomaly_detection', epochs=2)
+                                     data_dir=r'E:\Anomaly_detection', epochs=20)
     if VGG:
-        run_VGG_experiments(layersvgg, filtersvgg, (640, 360), batch_size=8,
-                            data_dir=r'E:\Anomaly_detection', epochs=10)
+        run_VGG_experiments(layersvgg, filtersvgg, kernels=kernels, image_size=(640, 360), batch_size=8,
+                            data_dir=r'E:\Anomaly_detection', epochs=20)
+    if ResNet:
+        run_ResNet_experiments(num_blocks, num_layers, num_filters, image_size=(640, 360), batch_size=8,
+                               data_dir=r'E:\Anomaly_detection', epochs=20)
