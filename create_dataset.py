7d00403669e71077406ff830ce22e0d596ad1afc
marnixteunissen <70704360+marnixteunissen@users.noreply.github.com>
2021-08-12 15:44:55 +0200

    updated all dataset creation functions, tested on tulip oil, works for multiple classes

--- create_dataset.py
+++ create_dataset.py
@@ -2,18 +2,9 @@ from file_functions import *
 from excel_functions import *
 from opencv_functions import *
 import os
+from random import shuffle
+from shutil import move
 import argparse
-
-
-def get_delays():
-    delays = {'LingShui':           1.900,
-              'Troll':              1.550,
-              'Turkstream':         0.500,
-              'Baltic Connector':   0.000,
-              'Noble Tamar':        2.850,
-              'Nordstream':         1.200,
-              'Sur de Texas':       0.000}
-    return delays
 
 
 def create_dataset(project,
@@ -26,12 +17,6 @@ def create_dataset(project,
                    split=0.15,
                    use_perc=1.0,
                    root_dir=r'K:\PROJECTS\SubSea Detection\12 - Data'):
-
-    # Check if project is available:
-    delay_dict = get_delays()
-
-    if project not in delay_dict.keys():
-        raise FileNotFoundError('Project files were not found in {}'.format(root_dir))
 
     # Getting correct channel names:
     channels, ch_str = ch(project, channel_idx)
@@ -96,8 +81,8 @@ def create_dataset(project,
 
             for video_dir in dirs:
                 # extracting all the frames from the video (only Field Joints for now)
-                extract_all_pos_frames(project, video_dir, data_points, output_dir,
-                                       delay=delay, channel=channel, n_augment=extra_pos_samples)
+                extract_all_event_frames(project, video_dir, data_points, output_dir,
+                                         delay=delay, channel=channel, n_augment=extra_pos_samples)
                 tot_neg = (extra_pos_samples + 1) * neg_samples
                 extract_all_neg_frames(project, video_dir, data_points, output_dir,
                                        nr_samples=tot_neg, delay=delay, channel=channel)
@@ -105,17 +90,108 @@ def create_dataset(project,
     print('Data-set created for project', project)
 
 
+def create_new_dataset(project,
+                       channel_idx=1,
+                       classes=['FJOK'],
+                       delay=0.000,
+                       neg_samples=5,
+                       extra_pos_samples=0,
+                       out_dir='',
+                       test_split=0.15,
+                       root_dir=r'K:\PROJECTS\SubSea Detection\12 - Data'):
+
+    # Getting correct channel names:
+    channels, ch_str = ch(project, channel_idx)
+
+    # Check if root directory exists:
+    if not os.path.exists(root_dir):
+        raise FileExistsError('specified root directory does not exist')
+
+    # Setting project directory
+    project_dir = os.path.join(root_dir, project)
+    if os.path.exists(project_dir):
+        print('Extracting data samples from project:', project)
+    else:
+        raise FileNotFoundError('Specified project not found in', root_dir)
+
+    # Setting output directory:
+    yes = {'yes', 'y', 'ye', ''}
+
+    if out_dir == '' and os.path.exists(os.path.join(root_dir, 'data-set')):
+        save_dir = os.path.join(root_dir, 'data-set')
+        create_channel_folders(save_dir, classes)
+
+    elif out_dir == '' and not os.path.exists(os.path.join(root_dir, 'data-set')):
+        choice = input("Would you like to create a 'data-set' folder in '{}'?".format(root_dir)).lower()
+        if choice in yes:
+            save_dir = os.path.join(root_dir, 'data-set')
+            os.mkdir(save_dir)
+            create_channel_folders(save_dir, classes)
+        else:
+            raise Exception('Data-set directory was not created, no data was extracted.')
+
+    elif os.path.exists(out_dir):
+        save_dir = out_dir
+        create_channel_folders(save_dir, classes)
+
+    else:
+        choice = input('Specified output directory not found, would you like to create it? (y)/(n)')
+        if choice in yes:
+            os.mkdir(out_dir)
+            save_dir = out_dir
+            create_channel_folders(save_dir)
+
+        else:
+            raise Exception('Specified save directory not found, and not created')
+
+    print('Saving dataset to:', save_dir)
+
+    data_points = get_event_types(project_dir, event_types=classes)
+    data_dirs = get_video_dirs(project_dir)
+
+    # Create the full data-set in train folder:
+    print('channels:', str(ch_str))
+    for channel_string, channel in zip(ch_str, channels):
+        channel_dir = os.path.join(save_dir, channel_string)
+        for dir in data_dirs:
+            output_dir = os.path.join(channel_dir, 'train')
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            extract_all_event_frames(project, dir, data_points, output_dir,
+                                     delay=delay, channel=channel, n_augment=extra_pos_samples)
+            tot_neg = (extra_pos_samples + 1) * neg_samples
+            extract_all_neg_frames(project, dir, data_points, output_dir,
+                                   nr_samples=tot_neg, delay=delay, channel=channel)
+
+        # move part of data to test folder:
+        print('Moving test files for {} channel'.format(channel_string))
+        for cl in classes:
+            samples = os.listdir(os.path.join(channel_dir, 'train', cl))
+            n_samples = len(samples)
+            print(n_samples)
+            n_test = int(test_split * n_samples)
+            print(n_test)
+            shuffle(samples)
+            for file in samples[:n_test]:
+                source = os.path.join(channel_dir, 'train', cl, file)
+                destination = os.path.join(channel_dir, 'test', cl, file)
+                # move file
+                move(source, destination)
+
+
 if __name__ == "__main__":
     # TODO: add parser arguments for commandline running
-    delays = {'LingShui':                     1.900,
-              'Troll':                        2.250,
-              'Turkstream':                   0.500,
-              'Baltic Connector':             0.500,
-              'Noble Tamar':                  2.850,
-              'Nordstream':                   1.200,
-              'Sur de Texas':                 0.000}
+    delays = {'LingShui':           1.900,
+              'Troll':              2.250,
+              'Turkstream':         0.500,
+              'Baltic Connector':   0.500,
+              'Noble Tamar':        2.850,
+              'Nordstream':         1.200,
+              'Sur de Texas':       0.000,
+              'Tulip Oil':          1.600}
 
-    root = 'K'
+    dataset_dir = r'E:\dataset_22_07_21'
+    root = 'Other'
     if root == 'H':
         # for projects on H: drive
         projects = ['Baltic Connector', 'Noble Tamar', 'Nordstream']
@@ -128,15 +204,28 @@ if __name__ == "__main__":
         extras = [11, 7]
         negatives = [6, 6]
         data_dir = r'K:\PROJECTS\SubSea Detection\12 - Data'
+    elif root == 'Other':
+        projects = ['Tulip Oil']
+        extras = [0]
+        negatives = [0]
+        data_dir = r'C:\Users\MTN\Documents\Anomaly_Detection\Data'
     else:
         raise ValueError('Wrong root')
 
-    dataset_dir = r'E:\dataset_22_07_21'
-    for project, extra, negative in zip(projects, extras, negatives):
-        delay = delays[project]
-        create_dataset(project,
-                       delay=delay,
-                       neg_samples=negative,
-                       extra_pos_samples=extra,
-                       root_dir=data_dir,
-                       out_dir=dataset_dir)
+    create_new_dataset(projects[0],
+                       channel_idx=1,
+                       classes=['FJOK'],
+                       delay=delays[projects[0]],
+                       neg_samples=negatives[0],
+                       extra_pos_samples=extras[0],
+                       out_dir='',
+                       test_split=0.15,
+                       root_dir=data_dir)
+    # for project, extra, negative in zip(projects, extras, negatives):
+    #     delay = delays[project]
+    #     create_dataset(project,
+    #                    delay=delay,
+    #                    neg_samples=negative,
+    #                    extra_pos_samples=extra,
+    #                    root_dir=data_dir,
+    #                    out_dir=dataset_dir)
--- excel_functions.py
+++ excel_functions.py
@@ -44,8 +44,8 @@ def extract_video_events(excel_data, video, static_offset=0.000):
     Extracts the timestamps with labels from the events sheet.
     :param excel_data:      Pandas DataFrame (not dict of DataFrames)
                             Data extracted from the excel sheet with the
-                            extract_excel_data function, one label only.
-    :param video:    Path string
+                            extract_excel_data function.
+    :param video:           Path string
                             Path to folder where video feeds are located
     :param static_offset:   float
                             offset in seconds
@@ -80,70 +80,93 @@ def extract_video_events(excel_data, video, static_offset=0.000):
 
     # Create pandas DataFrame with DateTime and labels from Secondary Code
     # for all events in excel_data:
-    timestamps = excel_data[((first_stamp <= excel_data['datetime']) & (excel_data['datetime'] <= last_stamp))][['datetime', 'Secondary Code']]
+    timestamps = excel_data[((first_stamp <= excel_data['datetime']) & (excel_data['datetime'] <= last_stamp))][['datetime', 'code']]
     # Calculating elapsed time in video
     offset = timedelta(seconds=static_offset)
     timestamps['Video Stamp'] = (timestamps['datetime'] - first_stamp + offset)
     timestamps['ms in video'] = (timestamps['Video Stamp'].dt.total_seconds() * 1000).astype('int')
 
-    data_points = timestamps[['datetime', 'Video Stamp', 'ms in video', 'Secondary Code']]
+    data_points = timestamps[['datetime', 'Video Stamp', 'ms in video', 'code']]
 
     return data_points
 
 
 def extract_all_events(project_dir):
     # Find excel sheet in project folder:
-    print('Opening .xlsx file...')
     dir_files = os.listdir(project_dir)
     excel_files = [os.path.join(project_dir, file) for file in dir_files if file.endswith('.xlsx')]
-    assert len(excel_files) == 1
+    assert len(excel_files) == 1, "{} available excel files were found, " \
+                                  "make sure one single excel file is available in the project directory".format(len(excel_files))
     # TODO: ask for user input if len(excel_files) != 0
+    excel_file = excel_files[0]
 
-    full_excel = pd.read_excel(excel_files[0], None)
-    print('Opened', excel_files[0])
+    xlsx = pd.ExcelFile(excel_file)
+    event_sheets = [name for name in xlsx.sheet_names if (name.lower().count('event') != 0
+                                                          or name.lower().count('observation') != 0)]
 
-    # find the sheet with all events:
-    sheet_names = full_excel.keys()
-    event_sheets = [name for name in sheet_names if (name.lower().count('event') != 0
-                                                    or name.lower().count('observation') != 0)]
     assert len(event_sheets) == 1
     # TODO: user selection of appropriate sheet with all events if assertion fails
-
-    event_sheet = full_excel[event_sheets[0]]
-    columns = event_sheet.keys()
+    event_sheet = event_sheets[0]
 
     if project_dir.endswith('Troll'):
-        columns_of_interest = ['date', 'time', 'secondary code']
+        columns_of_interest = ['date', 'time', 'secondarycode']
     else:
-        columns_of_interest = ['date', 'time', 'primary code', 'secondary code']
+        columns_of_interest = ['date', 'time', 'primarycode', 'secondarycode']
+
+    # Open event sheet:
+    print('Opening excel sheet with events...')
+    full_event_sheet = pd.read_excel(excel_file, sheet_name=event_sheet, dtype=str)
 
     # Copy the relevant columns to new dataframe:
     columns_to_copy = []
+    original_columns = full_event_sheet.keys()
+    clean_columns = [col.replace(" ", "").replace("\n", "").lower() for col in original_columns]
+
     for col in columns_of_interest:
-        columns_to_copy.extend([column for column in columns if column.lower().startswith(col)])
+        columns_to_copy.extend([column for column in clean_columns if column.startswith(col)])
 
-    events = event_sheet[columns_to_copy]
-
-    # for debugging:
-    for col in columns_to_copy:
-        print(type(events[col][0]))
+    full_event_sheet.columns = clean_columns
+    events = full_event_sheet[columns_to_copy]
 
     # renaming columns:
     events.columns = columns_of_interest
-    print(events.keys())
 
-    if project.endswith('Turkstream'):
-        events['date'] = [d.date() for d in events['date']]
+    for col in events.keys()[:2]:
+        events[col] = pd.to_datetime(events[col], infer_datetime_format=True, errors='coerce')
+        events.dropna(subset=[col], inplace=True)
 
-    return events
+    events['datetime'] = events.apply(lambda r: datetime.combine(r['date'].date(), r['time'].time()), 1)
+
+    if project_dir.endswith('Troll'):
+        events['code'] = events['secondarycode']
+    else:
+        events.loc[:, 'code'] = events.loc[:, 'primarycode'] + events.loc[:, 'secondarycode']
+
+    return events[['datetime', 'code']]
+
+
+def select_events(events, event_types=['FJOK']):
+    selected_events = events.loc[events['code'].isin(event_types)]
+    return selected_events
+
+
+def get_event_types(project_dir, event_types=['FJOK']):
+    events = extract_all_events(project_dir)
+    return select_events(events, event_types)
 
 
 if __name__ == "__main__":
-    dir = os.path.join(os.getcwd(), 'data')
+    #dir = os.path.join(os.getcwd(), 'data')
+    dir = r'C:\Users\MTN\Documents\Anomaly_Detection\Data'
     print('Working Dir:', dir)
-    project = 'Troll'
-    proj_dir = os.path.join(dir, project)
-    event_list = extract_all_events(proj_dir)
+    projects = os.listdir(dir)
+    print(projects)
+    for project in ['Tulip Oil']:
+        print('converting project {}'.format(project))
+        proj_dir = os.path.join(dir, project)
+        eventing_list = get_event_types(proj_dir, ['FJOK'])
+        #eventing_list.to_csv(os.path.join(proj_dir, 'Events {}.csv'.format(project)), index=False)
+        print('project converted.')
 
-
-
+    vid_events = extract_video_events(eventing_list, r'C:\Users\MTN\Documents\Anomaly_Detection\Data\Tulip Oil\Video\DATA_20180620122949270', static_offset=0.000)
+    print(vid_events)
