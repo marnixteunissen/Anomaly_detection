937a1f8e3377b8ca3d90624e7ad0e0e86f5e3e01
marnixteunissen <70704360+marnixteunissen@users.noreply.github.com>
2021-08-16 15:28:32 +0200

    latest changes to retrain for Tulip Oil project

--- retrain.py
+++ retrain.py
@@ -1,18 +1,27 @@
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras import layers, Input, Model
-from os import listdir
-from os.path import join
+from tensorflow.keras import layers, Input, Model, losses, optimizers
+from os import listdir, makedirs
+from os.path import join, exists
 import argparse
 import excel_functions as ex
 import data_processing as data
+from time import time
+from datetime import datetime
 import train
+from shutil import copy
 
 
-def retrain(model_dir, data_dir='', save_dir='', batch_size=32, epochs=30):
+def retrain(model_dir, data_dir='', batch_size=32, epochs=50, optimizer='adam'):
     # TODO: get config from file
     classes = 2
     img_size = (360, 640)
+    now = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    ret_dir = model_dir + '/retrain/' + now
+    makedirs(ret_dir)
+    conf = model_dir + '/config.json'
+    conf_dest = ret_dir + '/config.json'
+    copy(conf, conf_dest)
 
     # create new dataset
     train_set, val_set = train.create_datasets(data_dir, img_size, batch_size)
@@ -31,12 +40,12 @@ def retrain(model_dir, data_dir='', save_dir='', batch_size=32, epochs=30):
         else:
             old_model = load_model(model_dir + '/saved_model')
 
-    base_model = tf.keras.models.Model(inputs=old_model.inputs, outputs=old_model.layers[-5].get_output_at(0))
+    base_model = tf.keras.models.Model(inputs=old_model.inputs, outputs=old_model.layers[-4].get_output_at(0))
     # Freeze layers
     base_model.trainable = False
 
     # Create new top layers
-    pool_layer = layers.GlobalAvgPool2D()
+    pool_layer = layers.GlobalMaxPool2D()
     dense_layer = layers.Dense(128, activation='relu')
     prediction_layer = layers.Dense(classes, activation='relu')
     outputs = layers.Softmax()
@@ -44,29 +53,36 @@ def retrain(model_dir, data_dir='', save_dir='', batch_size=32, epochs=30):
     # Create new model
     inputs = Input(shape=(img_size[0], img_size[1], 3))
     x = base_model(inputs, training=False)
-    x = pool_layer(x)
+    # x = pool_layer(x)
     x = dense_layer(x)
     x = prediction_layer(x)
     output = outputs(x)
 
     new_model = Model(inputs, output)
+    optimizer = optimizers.Adam(amsgrad=True)
+    new_model.compile(optimizer=optimizer,
+                      loss=losses.SparseCategoricalCrossentropy(),
+                      metrics=['accuracy'])
     new_model.summary()
 
     # setup for training:
     # create callback to save best model:
     save_best = tf.keras.callbacks.ModelCheckpoint(
-        filepath=join(save_dir, 'saved_model/best/best_model.h5'),
+        filepath=join(ret_dir, 'saved_model', 'best', 'best_model.h5'),
         save_weights_only=False,
         monitor='val_accuracy',
         mode='max',
         save_best_only=True)
     # add early stopping criterion:
-    early_stop = tf.keras.callbacks.EarlyStopping(patience=8)
+    early_stop = tf.keras.callbacks.EarlyStopping(patience=5)
 
     # Start training
+    start_time = time()
     history = new_model.fit(train_set, validation_data=val_set, epochs=epochs, callbacks=[save_best, early_stop])
-    new_model.save(join(save_dir, 'saved_model/last_model.h5'))
-    train.save_losses(history, save_dir)
+    finish_time = time()
+    print("Total training time: {}".format(finish_time-start_time))
+    new_model.save(join(ret_dir, 'saved_model', 'last_model.h5'))
+    train.save_losses(history, (ret_dir + '/losses.png'))
     new_model.evaluate(test_set)
 
 
@@ -79,8 +95,5 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default=None,
                         help="Full path to the directory which contains the 'saved_model' directory",
                         required=False)
-    parser.add_argument('--save_dir', type=str, default=None,
-                        help='Full path to location to store the new model')
     opt = parser.parse_args()
-    path = r'K:\PROJECTS\SubSea Detection\10 - Development\Training Results\29-07-2021\Varying layers and filters\1'
-    retrain(path)
+    retrain(opt.model, opt.data, batch_size=16)
